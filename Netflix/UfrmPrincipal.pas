32df47108a44152dfd6e0a6ce528175c2df08540
Jean Carlos Möller <jeancmoller17@gmail.com>
2022-12-12 21:56:42 -0300

    dando continuidade com clone netflix

--- Netflix/UfrmPrincipal.pas
+++ Netflix/UfrmPrincipal.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.ListBox, FMX.Ani;
 
 type
   TForm1 = class(TForm)
@@ -26,8 +26,18 @@ type
     Image9: TImage;
     Label3: TLabel;
     Label4: TLabel;
+    LayoutMenu: TLayout;
+    LayoutFechar: TLayout;
+    Rectangle1: TRectangle;
+    Image10: TImage;
+    ListBox1: TListBox;
+    FloatAnimation1: TFloatAnimation;
   private
     { Private declarations }
+
+    procedure LoadMenu;
+    procedure OpenMenu(ind: Boolean);
+    procedure SetupMenu(item: TListBoxItem; texto: string);
   public
     { Public declarations }
   end;
@@ -38,5 +48,59 @@ var
 implementation
 
 {$R *.fmx}
+
+{ TForm1 }
+
+procedure TForm1.LoadMenu;
+begin
+  ListBox1.Items.Clear;
+
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Todos os gêneros');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Disponível para dowload');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Ação');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Adolecente');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Anime');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Asiáticos');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Brasileiros');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Comédia');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Drama');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Esportes');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Mistério');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Novelas');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Suspense');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Terror');
+
+end;
+
+procedure TForm1.OpenMenu(ind: Boolean);
+begin
+  //Esconde o item selecionado..
+  ListBox1.ItemIndex := 1;
+
+  //volta a listagem para o inicio
+  ListBox1.ScrollToItem(ListBox1.ItemByIndex(0));
+
+  if ind then
+  begin
+    Layout1.Visible := True;
+    Layout2.Tag     := 1;
+    Layout3.AnimateFloat('Opacity', 0, 0.2);
+    FloatAnimation1.Inverse := False;
+
+  end
+  else
+  begin
+     Layout2.Tag := 0;
+     Layout3.AnimateFloat('Opacity', 1, 0.4);
+     FloatAnimation1.Inverse := False;
+  end;
+    FloatAnimation1.Start;
+
+end;
+
+procedure TForm1.SetupMenu(item: TListBoxItem; texto: string);
+begin
+
+end;
 
 end.
