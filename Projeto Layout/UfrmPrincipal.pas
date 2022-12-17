4c44769d57198e5ba9d42059fccd42c0e6c7c399
FURB\jcmoller <jeancmoller17@gmail.com>
2022-12-16 21:54:55 -0300

    atividades

---
+++ Projeto Layout/UfrmPrincipal.pas
@@ -0,0 +1,34 @@
+unit UfrmPrincipal;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Layouts;
+
+type
+  TForm1 = class(TForm)
+    FlowLayout1: TFlowLayout;
+    FlowLayout8: TFlowLayout;
+    Rectangle2: TRectangle;
+    Rectangle3: TRectangle;
+    Rectangle4: TRectangle;
+    Rectangle5: TRectangle;
+    Rectangle6: TRectangle;
+    Rectangle7: TRectangle;
+    Image1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
